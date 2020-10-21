7ac73ad1c5a24aa4a49ba1a6c9ab03907d6505e0
JQueen <queen_jenf@yahoo.com>
2020-10-20 18:36:39 -0700

    update partial folder

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const express = require('express');
-const routes = require('./controllers');
+const session = require("express-session");
+const hbs = require('express-handlebars'); 
 const sequelize = require('./config/connection')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = require('express-handlebars'); 
-const session = require("express-session");
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(
+  session({
+    secret: "asfd09aq8w7r",
+    db: sequelize,
+    resave: false,
+  })
+);
+app.use(routes);
 app.engine('handlebars', hbs());
 app.set('view engine', 'handlebars');
-
-app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
