0e5b238988ca86bd3866b71ea160f205e9025727
JQueen <queen_jenf@yahoo.com>
2020-10-24 10:53:24 -0700

    updates

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ const Sequelize = require('sequelize');
 // Create connection to our database, pass in your MySQL information for username and password
 let sequelize;
 
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
